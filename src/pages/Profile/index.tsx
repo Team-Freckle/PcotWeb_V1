fd0e0decb345255f68cc53666ed75b24a0524b28
Jinsoo1004 <jinsoo1004@proton.me>
2023-08-28 23:47:07 +0900

    🔨 :: Profile 페이지 개발중

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,36 +1,32 @@
 import React from "react";
-// import { useNavigate } from "react-router-dom";
-import { ReactComponent as Reservation } from "../../assets/Login-Logo.svg";
-import { ReactComponent as Profilechange } from "../../assets/dummy_profilechange_icon.svg";
 
-// import styles from '../../style/profile-style.module.css';
 import * as S from "./style";
 import "../../styles/styles.css";
 
+import ProfileImg from "../../assets/profile.svg";
+
 const Profile = () => {
-  // const navigate = useNavigate();
-  // const LoginHandel = () => {
-  //     navigate("/profile");
-  // }
   return (
-    <>
-      <S.Flex>
-        <S.Form>
-          <S.Text>Profile</S.Text>
-          <S.ImgBox>
-            <Reservation className={"profileLogoImg"} />
-            <Profilechange className={"profileChangeButton"} />
-          </S.ImgBox>
-          <S.ProfileId>
-            dummy_id
-            <br />
-            <S.ProfileEmail>dummy_email@gmail.com</S.ProfileEmail>
-          </S.ProfileId>
-          <S.SaveLoginButton>저장하고 로그인하기</S.SaveLoginButton>
-          <S.LaterButton>나중에 할게요</S.LaterButton>
-        </S.Form>
-      </S.Flex>
-    </>
+    <div>
+      <h1>Profile</h1>
+      <img src={ProfileImg} />
+      <S.Inputs>
+        <S.InputBox>
+          <S.InputText>Name</S.InputText>
+          <S.Input type="text" />
+        </S.InputBox>
+
+        <S.InputBox>
+          <S.InputText>Email</S.InputText>
+          <S.Input type="text" />
+        </S.InputBox>
+
+        <S.InputBox>
+          <S.InputText>Bio</S.InputText>
+          <S.Input type="text" />
+        </S.InputBox>
+      </S.Inputs>
+    </div>
   );
 };
 
--- src/pages/Profile/style.ts
+++ src/pages/Profile/style.ts
@@ -1,138 +1,28 @@
 import styled from "styled-components";
 
-export const Flex = styled.div`
+export const Inputs = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
   flex-direction: column;
-  height: 100vh;
+  margin-top: 30px;
 `;
 
-export const Form = styled.form`
-  background: #ffffff;
-  box-shadow: 1px 1px 20px rgba(183, 183, 183, 0.2);
-  border-radius: 10px;
-  @media screen and (min-width: 600px) {
-    margin: auto;
-    width: 550px;
-    height: 800px;
-  }
-  @media screen and (max-width: 600px) {
-    margin: 0 auto;
-    margin-top: -30%;
-    width: 90%;
-    height: 80%;
-  }
-`;
-
-export const Text = styled.div`
-  font-family: "Pretendard";
-  font-style: normal;
-  font-weight: 900;
-  font-size: 46px;
-  line-height: 135.5%;
-  color: #316ae2;
-  @media screen and (min-width: 600px) {
-    margin-top: 45px;
-    margin-bottom: 70px;
-  }
-  @media screen and (max-width: 600px) {
-    margin-top: 2%;
-    margin-bottom: 40px;
-  }
-`;
-
-export const ImgBox = styled.div`
-  margin: 0 auto;
-  @media screen and (min-width: 600px) {
-    margin-top: 10px;
-    width: 250px;
-    height: 250px;
-    background-color: #f2f2f2;
-    border-radius: 50%;
-  }
-  @media screen and (max-width: 600px) {
-    width: min-content;
-    height: min-content;
-    background-color: #f2f2f2;
-    border-radius: 50%;
-    align-items: center;
-  }
-`;
-
-export const ProfileId = styled.div`
-  margin-bottom: 8px;
-  @media screen and (min-width: 600px) {
-    margin-top: 20px;
-    font-size: 25px;
-  }
-  @media screen and (max-width: 600px) {
-    margin-top: 3%;
-    font-size: 20px;
-  }
-`;
-
-export const ProfileEmail = styled.span`
-  color: gray;
-  @media screen and (min-width: 600px) {
-    font-size: 20px;
-  }
-  @media screen and (max-width: 600px) {
-    font-size: 13px;
-  }
-`;
-
-export const SaveLoginButton = styled.button`
-  border: none;
-  width: 95%;
-  background: #316ae2;
-  color: #ffffff;
+export const Input = styled.input`
+  width: 300px;
+  height: 30px;
   border-radius: 5px;
-  cursor: pointer;
-  line-height: 135.5%;
-  text-align: center;
-  font-family: "Pretendard";
-  font-style: normal;
-  font-weight: 600;
-  @media screen and (min-width: 600px) {
-    margin-top: 90px;
-    height: 73px;
-    font-size: 20px;
-  }
-  @media screen and (max-width: 600px) {
-    margin-top: 30px;
-    height: 60px;
-    font-size: 15px;
-  }
-  :active {
-    background: #585858;
-    cursor: pointer;
-  }
+  border: 1px solid #ccc;
+  margin-bottom: 10px;
+  padding-left: 10px;
 `;
 
-export const LaterButton = styled.button`
-  border: 2px solid #316ae2;
-  margin-top: 10px;
-  width: 95%;
-  background: #ffffff;
-  color: #316ae2;
-  border-radius: 5px;
-  cursor: pointer;
-  font-family: "Pretendard";
-  font-style: normal;
-  font-weight: 600;
-  line-height: 135.5%;
-  text-align: center;
-  @media screen and (min-width: 600px) {
-    height: 73px;
-    font-size: 20px;
-  }
-  @media screen and (max-width: 600px) {
-    height: 60px;
-    font-size: 15px;
-  }
-  :active {
-    background: #585858;
-    cursor: pointer;
-  }
+export const InputBox = styled.div`
+  position: relative;
+  width: 300px;
+  margin-top: 30px;
+`;
+
+export const InputText = styled.div`
+  position: absolute;
+  bottom: 50px;
+  left: 0;
 `;
