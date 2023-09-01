e8e49ea0564e631dd979d163d8896b88a45098b9
Jinsoo1004 <jinsoo1004@proton.me>
2023-09-01 10:06:18 +0900

    🔨 :: Profile페이지 header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,31 +3,36 @@ import { useNavigate } from "react-router-dom";
 
 import * as S from "./style";
 
-import Banner from "@assets/banner.png";
 import Profile from "@assets/Login-Logo.svg";
 import Serch from "@assets/search.svg";
 
 export const Home = () => {
   const navigate = useNavigate();
   return (
-    <div>
+    <S.Container>
       <S.ProfileBox>
         <S.ProfileImg src={Profile} />
-        <S.NameText>김경민 회원님</S.NameText>
-        <S.LinkText
-          onClick={() => {
-            navigate("/profile");
-          }}
-        >
-          프로필 보러가기 &gt;
-        </S.LinkText>
+        <div>
+          <S.NameText>김경민 회원님</S.NameText>
+          <S.LinkText
+            onClick={() => {
+              navigate("/profile");
+            }}
+          >
+            프로필 보러가기
+            &gt;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </S.LinkText>
+        </div>
+      </S.ProfileBox>
+
+      <S.InputBox>
         <S.InputLabel>
           <S.SerchInput placeholder="검색어를 입력해주세요." />
           <S.InputButton src={Serch} />
         </S.InputLabel>
-      </S.ProfileBox>
+      </S.InputBox>
 
       {/* <img src={Banner} style={{ width: "80%", height: "30%" }}></img> */}
-    </div>
+    </S.Container>
   );
 };
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,16 +1,57 @@
 import styled from "styled-components";
 
-export const ProfileBox = styled.div`
+export const Container = styled.div`
   display: grid;
-  width: 100vh;
-  grid-template-columns: 200px 200px 500px 200px;
+  grid-template-rows: repeat(3, 200px);
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-areas:
+    "header . serch"
+    "main main aside"
+    "footer footer footer";
+  @media (max-width: 768px) {
+    grid-template-rows: repeat(3, 200px);
+    grid-template-columns: repeat(1, 1fr);
+    grid-template-areas:
+      "serch"
+      "main"
+      "footer";
+  }
 `;
 
-export const ProfileImg = styled.img``;
+export const ProfileBox = styled.div`
+  grid-area: header;
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
 
-export const NameText = styled.div``;
+export const ProfileImg = styled.img`
+  width: 130px;
+  height: 150px;
+  float: left;
+  margin-left: 20px;
+`;
 
-export const LinkText = styled.div``;
+export const NameText = styled.div`
+  width: 400px;
+  font-family: Pretendard;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin-top: 50px;
+`;
+
+export const LinkText = styled.div`
+  width: 400px;
+  color: #7a7a7a;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  cursor: pointer;
+`;
 
 export const SerchInput = styled.input`
   width: 300px;
@@ -19,15 +60,26 @@ export const SerchInput = styled.input`
   border-radius: 30px;
   padding-left: 20px;
   box-shadow: 1px 1px 20px 0px rgba(183, 183, 183, 0.387);
+  float: right;
 `;
 
 export const InputLabel = styled.label`
+  display: block;
   position: relative;
-  width: 300px;
+`;
+
+export const InputBox = styled.div`
+  grid-area: serch;
+  margin-top: 50px;
+  width: 400px;
+  margin-right: 40px;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 export const InputButton = styled.img`
   position: absolute;
-  right: 0;
   top: 8px;
+  right: 20px;
 `;
