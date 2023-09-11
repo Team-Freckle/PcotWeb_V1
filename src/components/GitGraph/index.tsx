f6df1ffb376eb588267dfd3927712baa3d343715
kimkmin06 <kimkmin06@gmail.com>
2023-09-11 20:46:19 +0900

    🔨 :: Gitgraph 노드 추가 버튼 개발 중

--- src/components/GitGraph/index.tsx
+++ src/components/GitGraph/index.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { Gitgraph, templateExtend, TemplateName, Orientation } from "@gitgraph/react";
 
 export const GitGraph = () => {
+  const [mkNode, setMkNode] = useState(0);
+  // console.log(mkNode);
+
   const withoutAuthor = templateExtend(TemplateName.Metro, {
     commit: {
       message: {
@@ -12,27 +15,33 @@ export const GitGraph = () => {
   });
 
   const initGraph = (gitgraph: any) => {
-    // const [mkBranch, setMkBranch] = useState();
-    // const [mkNode, setMkNode] = useState(0);
+    const main = gitgraph.branch("main").commit("zero");
+  };
 
-    const main = gitgraph.branch("main").commit("Initial commit");
-    main.commit("업데이트 내용");
-    const user2 = main.branch("user2");
-    user2.commit("업데이트 내용2");
-    const user3 = main.branch("user3");
-    user3.commit("업데이트 내용3");
-    main.commit("업데이트 내용4");
-    main.merge(user2);
-    main.merge(user3);
-    main.commit("업데이트 내용6");
+  const node = () => {
+    if (mkNode == 1) {
+      console.log("gooooooooood");
+      setMkNode(0);
+    }
   };
 
   return (
-    <Gitgraph options={{ template: withoutAuthor, orientation: Orientation.VerticalReverse }}>
-      {(gitgraph) => {
-        initGraph(gitgraph);
+    <div>
+      <button
+        onClick={() => {
+          node();
+          setMkNode(1);
+        }}
+      >
+        노드 추가
+      </button>
+      {/* <Gitgraph options={{ template: withoutAuthor, orientation: Orientation.VerticalReverse }}>
+        {(gitgraph) => {
+        initGraph(gitgraph);0
       }}
-    </Gitgraph>
+        {initGraph.bind(this)}
+      </Gitgraph> */}
+    </div>
   );
 };
 
