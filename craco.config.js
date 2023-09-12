0017de3dcf7f8cc57f0e466eade0ce640e0eb5f5
Jinsoo1004 <jinsoo1004@proton.me>
2023-09-12 20:39:36 +0900

    수정

--- craco.config.js
+++ craco.config.js
@@ -1,24 +1,8 @@
-<<<<<<< HEAD
-const path = require("path");
-=======
 const CracoAlias = require("craco-alias");
->>>>>>> main
 
 module.exports = {
   plugins: [
     {
-<<<<<<< HEAD
-      plugin: require("@semantic-ui-react/craco-less"),
-    },
-  ],
-  webpack: {
-    alias: {
-      // Add any Semantic UI LESS variables overrides here
-      // For example, to change the primary color:
-      // 'site': path.resolve(__dirname, 'src/site.less'),
-    },
-  },
-=======
       plugin: CracoAlias,
       options: {
         source: "tsconfig",
@@ -27,5 +11,4 @@ module.exports = {
       },
     },
   ],
->>>>>>> main
 };
