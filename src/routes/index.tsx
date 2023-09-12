62c965674159d3f0132551403f3a3d4b4c64b44f
Jinsoo1004 <jinsoo1004@proton.me>
2023-09-12 20:42:10 +0900

    수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,12 @@
 import React from "react";
+
 import { Route, Routes } from "react-router-dom";
-import { Login } from "../components/Auth/Login";
-import { Signin } from "../components/Auth/Signin";
-
-import { NotFound } from "../components/NotFound";
-import { Home } from "../pages/Home";
-import { EmailVerify } from "../pages/EmailVerify";
-import SideBar from "../components/SideBar";
-// import SideBarWorkspace from "../components/SideBar/WorkSpace";
-import GitGraph from "../components/GitGraph";
-
-import MainSideBar from "../components/SideBar/MainSideBar";
-
-// import Test from "../components/SideBar/test";
+import { Login } from "@components/Auth/Login";
+import { Signin } from "@components/Auth/Signin";
+import Profile from "@pages/Profile";
+import { NotFound } from "@components/NotFound";
+import { Home } from "@pages/Home";
+import { EmailVerify } from "@pages/EmailVerify";
 
 export const Router = () => {
   return (
@@ -22,11 +16,8 @@ export const Router = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/EmailVerify" element={<EmailVerify />} />
-        <Route path="/side" element={<SideBar />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/side" element={<SideBar />} />
-        <Route path="/graph" element={<GitGraph />} />
       </Routes>
     </div>
   );
