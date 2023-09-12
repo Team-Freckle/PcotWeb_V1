5d9861e0d273709757ef580fca353ba9a81e2ead
Jinsoo1004 <jinsoo1004@proton.me>
2023-09-12 23:08:47 +0900

    수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import React from "react";
 import "./App.css";
 import { Router } from "./routes";
 import { BrowserRouter } from "react-router-dom";
-const styleLink = document.createElement("link");
-styleLink.rel = "stylesheet";
-styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
-document.head.appendChild(styleLink);
 
 function App() {
   return (
