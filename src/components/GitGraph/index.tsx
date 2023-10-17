fb9e3726ede05058514e1a2489f0bee91edb7ff8
kimkmin06 <kimkmin06@gmail.com>
2023-10-18 00:28:19 +0900

    🔨 :: node select box 개발 중

--- src/components/GitGraph/index.tsx
+++ src/components/GitGraph/index.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { Gitgraph, templateExtend, TemplateName, Orientation } from "@gitgraph/react";
+import Modal from "react-modal";
 
-import { Popup } from "./Popup/index.tsx";
+// import { Popup } from "./Modal/index.tsx";
 import * as S from "./style";
 import Serch from "@assets/plus.svg";
 
 export const GitGraph = () => {
-  const [isPopup, setIsPopup] = useState(false);
   const [mkNode, setMkNode] = useState(1);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   // console.log(mkNode);
 
   const withoutAuthor = templateExtend(TemplateName.Metro, {
@@ -24,30 +25,50 @@ export const GitGraph = () => {
     main.commit("업데이트1");
   };
 
-  const node = () => {
-    if (mkNode == 1) {
-      console.log("gooooooooood");
-      setMkNode(0);
-    }
-  };
-
-  const handlePopup = () => {
-    setIsPopup(true);
-  };
+  // const node = () => {
+  //   if (mkNode == 1) {
+  //     console.log("gooooooooood");
+  //     setMkNode(0);
+  //   }
+  // };
 
   return (
     <S.Container>
       <S.ContributesText>Node Graph</S.ContributesText>
       <S.Button
         onClick={() => {
-          handlePopup();
+          setModalIsOpen(true);
           // node();
           // setMkNode(1);
         }}
       >
         <img src={Serch} alt="Search" />
       </S.Button>
-      {/* {isPopup && <Popup ={setModalOpen} />} */}
+
+      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+        <S.ModalText>Node Name</S.ModalText>
+        <S.ModalInput />
+
+        <S.ModalText>Select Node</S.ModalText>
+        {/* <S.ModalInput /> */}
+        <S.SelectNode>
+          <S.OptionNode disabled selected>
+            노드선택하기
+          </S.OptionNode>
+          <S.OptionNode>A</S.OptionNode>
+          <S.OptionNode>B</S.OptionNode>
+          <S.OptionNode>C</S.OptionNode>
+          <S.OptionNode>D</S.OptionNode>
+          {/* <option disabled selected>
+            노드 선택하기
+          </option>
+          <option value="apple">apple</option>
+          <option value="orange">orange</option>
+          <option value="grape">grape</option>
+          <option value="melon">melon</option> */}
+        </S.SelectNode>
+        <S.ModalButton>Make node</S.ModalButton>
+      </Modal>
 
       <S.GraphBox>
         <Gitgraph options={{ template: withoutAuthor, orientation: Orientation.VerticalReverse }}>
--- src/components/GitGraph/style.ts
+++ src/components/GitGraph/style.ts
@@ -43,3 +43,104 @@ export const GraphBox = styled.div`
   margin-left: 70px;
   margin-top: 30px;
 `;
+
+export const SelectNode = styled.select`
+  width: 400px;
+  height: 60px;
+  border: none;
+  border-radius: 15px;
+  background-color: #e9e9e7;
+  margin-top: 10px;
+  margin-left: 20px;
+  padding-left: 20px;
+  padding-right: 20px;
+  /* font-size: inherit;
+  line-height: inherit; */
+  &:focus {
+    background-color: #e4ecff;
+  }
+`;
+
+export const OptionNode = styled.option`
+  width: 400px;
+  height: auto;
+  /* position: absolute; */
+  transform: translate(-50%, -50%);
+  border-radius: 15px;
+  background-color: #ffffff;
+`;
+
+export const ModalText = styled.div`
+  width: 400px;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  text-align: left;
+  margin-top: 20px;
+  margin-left: 35px;
+`;
+
+export const ModalInput = styled.input`
+  width: 400px;
+  height: 60px;
+  border: none;
+  border-radius: 15px;
+  background-color: #e9e9e7;
+  margin-top: 10px;
+  margin-left: 20px;
+  padding-left: 20px;
+`;
+
+export const ModalButton = styled.button`
+  width: 400px;
+  height: 60px;
+  border: none;
+  background: #316ae2;
+  border-radius: 15px;
+  color: #ffffff;
+  cursor: pointer;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-size: 20px;
+  line-height: 135.5%;
+  text-align: center;
+  margin-left: 20px;
+  margin-top: 10px;
+  /* @media screen and (min-width: 600px) {
+    width: 94%;
+    height: 73px;
+    color: #ffffff;
+  } */
+  :active {
+    background: #585858;
+    cursor: pointer;
+  }
+`;
+
+// const customModalStyles: ReactModal.Styles = {
+//   overlay: {
+//     backgroundColor: " rgba(0, 0, 0, 0.4)",
+//     width: "100%",
+//     height: "100vh",
+//     zIndex: 10,
+//     position: "fixed",
+//     top: 0,
+//     left: 0,
+//   },
+//   content: {
+//     width: "360px",
+//     height: "180px",
+//     zIndex: 150,
+//     position: "absolute",
+//     top: "50%",
+//     left: "50%",
+//     transform: "translate(-50%, -50%)",
+//     borderRadius: "10px",
+//     boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.25)",
+//     backgroundColor: "white",
+//     justifyContent: "center",
+//     overflow: "auto",
+//   },
+// };
