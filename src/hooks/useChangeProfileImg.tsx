02bedafbe93960c8436d3e48967b878f1746f8ce
Jinsoo1004 <kmjoplopl@gmail.com>
2023-10-18 13:41:22 +0900

    🔨 :: 프로필 사진 수정 에러 헨들링

--- src/hooks/useChangeProfileImg.tsx
+++ src/hooks/useChangeProfileImg.tsx
@@ -1,10 +1,10 @@
+import { ToastError } from "@lib/Toast";
 import axios from "axios";
 
 export const API_URL = process.env.REACT_APP_API;
 
 export const useChangeProfileImg = () => {
   const ProfileImgEditURL = `${API_URL}/v2/user/info/edit/profile`;
-  // const WorkspaceImgEditURL = `${API_URL}/v2/user/info/edit/workspace`;
   const onProfileImgSubmit = async (name: any, ProfileImg: any) => {
     try {
       const formData = new FormData();
@@ -17,8 +17,15 @@ export const useChangeProfileImg = () => {
           withCredentials: true,
         });
       }
-    } catch (error) {
-      console.log(error);
+    } catch (error: any) {
+      if (error.response.data.status === 400) {
+        ToastError("올바르지 않은 이미지 형식입니다.");
+      } else if (error.response.data.status === 500) {
+        ToastError("이미지의 용량이 너무 큽니다.\n5MB 이하의 이미지를 업로드해주세요.");
+      } else {
+        ToastError("프로필 이미지 수정에 실패했습니다.");
+        console.log(error);
+      }
     }
   };
   return { onProfileImgSubmit };
