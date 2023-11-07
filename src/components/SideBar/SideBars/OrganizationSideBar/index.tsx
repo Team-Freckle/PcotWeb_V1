e03372909c900d7805e49f3d4f48beb30e74043c
Jinsoo1004 <kmjoplopl@gmail.com>
2023-11-08 01:27:32 +0900

    사이드바 데이터 가져옴

--- src/components/SideBar/SideBars/OrganizationSideBar/index.tsx
+++ src/components/SideBar/SideBars/OrganizationSideBar/index.tsx
@@ -1,16 +1,52 @@
 import { SidebarHeader } from "@components/SideBar/SideBarHeader";
 import { Typography } from "@components/SideBar/Typography";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Sidebar } from "react-pro-sidebar";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import * as S from "./style";
 import addMemberBtn from "@assets/addMemberBtn.png";
+import { useGetOrganizationInfo } from "@hooks/useGetOrganizationInfo";
+import { useWorkspaceList } from "@hooks/useWorkspaceList";
+
+export const API_URL = process.env.REACT_APP_API;
 
 export const OrganizationSideBar = (props: any) => {
   const navigate = useNavigate();
+  const { organization } = useParams();
+  const [organizationInfo, setOrganizationInfo] = useState<any>([]);
+  const [organizationMember, setOrganizationMember] = useState<Array<any>>([]);
+  const [workspaceList, setWorkspaceList] = useState<Array<any>>([]);
+  const { serchOrganizationMamber, serchOrganizationMoreInfo } = useGetOrganizationInfo();
+  const { onRecentsListGet } = useWorkspaceList();
 
-  // const { Rank, setRank, Receiver, setReceiver } = userInvite();
+  useEffect(() => {
+    serchOrganizationMoreInfo(organization).then((res) => {
+      setOrganizationInfo(res.data);
+    });
+    serchOrganizationMamber(organization).then((res) => {
+      setOrganizationMember(
+        res.data.map((list: any) => (
+          <S.BottomMember
+            key={list.email}
+            src={`${API_URL}/v2/search/user/image?email=${list.email}`}
+            alt="Memberimg"
+          />
+        )),
+      );
+    });
+    onRecentsListGet("recents").then((res: any) => {
+      setWorkspaceList(
+        res.map((list: any) => (
+          <S.BottomWorkspace
+            key={list.createTime}
+            src={`${API_URL}/v2/search/workspace/image?organization=${list.organizationName}&workspace=${list.name}`}
+            onClick={() => navigate(`/workspace/${list.organizationName}/${list.name}`)}
+          />
+        )),
+      );
+    });
+  }, []);
 
   return (
     <div style={{ display: "flex", height: "100vh" }}>
@@ -38,10 +74,13 @@ export const OrganizationSideBar = (props: any) => {
               flex: 1,
             }}
           >
-            <S.Img src="https://via.placeholder.com/300" alt="testimg" />
+            <S.Img
+              src={`${API_URL}/v2/search/organization/image?name=${organizationInfo.name}`}
+              alt="Orgimg"
+            />
             <div style={{ marginTop: "20px" }}>
-              <Typography variant="subtitle1">Organization</Typography>
-              <S.Comment>테스트코맨트트트트트</S.Comment>
+              <Typography variant="subtitle1">{organizationInfo.name}</Typography>
+              <S.Comment>{organizationInfo.comment}</S.Comment>
             </div>
           </div>
           <S.BottomLayout>
@@ -50,18 +89,11 @@ export const OrganizationSideBar = (props: any) => {
                 Members
               </Typography>
               <S.BottomMembers>
-                <S.BottomMember src="https://via.placeholder.com/300" />
-                <S.BottomMember src="https://via.placeholder.com/300" />
-                <S.BottomMember src="https://via.placeholder.com/300" />
-                <S.BottomMember src="https://via.placeholder.com/300" />
-                <S.BottomMember src="https://via.placeholder.com/300" />
-                <S.BottomMember src="https://via.placeholder.com/300" />
-                <S.BottomMember src="https://via.placeholder.com/300" />
-                <S.BottomMember src="https://via.placeholder.com/300" />
-                <S.BottomMember src="https://via.placeholder.com/300" />
-                <S.BottomMember src="https://via.placeholder.com/300" />
-                <S.BottomMember src="https://via.placeholder.com/300" />
-                <S.BottomMember src="https://via.placeholder.com/300" />
+                {organizationMember}
+                {/* <S.BottomMember
+                  src={`${API_URL}/v2/search/user/image?email=test1@test.er`}
+                  alt="testimg"
+                /> */}
                 <S.BottomMember src={addMemberBtn} />
               </S.BottomMembers>
             </S.BottomItems>
@@ -69,12 +101,7 @@ export const OrganizationSideBar = (props: any) => {
               <Typography variant="subtitle1" style={{ marginRight: "auto", marginLeft: "20px" }}>
                 Other Workspace
               </Typography>
-              <S.BottomMembers>
-                <S.BottomWorkspace src="https://via.placeholder.com/300" />
-                <S.BottomWorkspace src="https://via.placeholder.com/300" />
-                <S.BottomWorkspace src="https://via.placeholder.com/300" />
-                <S.BottomWorkspace src="https://via.placeholder.com/300" />
-              </S.BottomMembers>
+              <S.BottomMembers>{workspaceList}</S.BottomMembers>
             </S.BottomItems>
           </S.BottomLayout>
         </div>
